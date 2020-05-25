2567e4e4a463cf74027ee4832e340c5de5604c4d
Mateusz Borycki <mateuszborycki@gmail.com>
2020-05-25 20:23:07 +0200

    Added luadebug.

--- src/interpreter.cpp
+++ src/interpreter.cpp
@@ -1,13 +1,14 @@
+#include "lua.hpp"
 #include <iostream>
 #include <string>
-#include <lua.hpp>
 
 bool interpret_string(const std::string& str)
 {
     auto L = luaL_newstate();
     luaL_openlibs(L);
     auto error = luaL_dostring(L, str.c_str());
-    if (error) {
+    if (error)
+    {
         auto msg = lua_tostring(L, -1);
         std::cout << "Lua error: " << msg;
     }
@@ -16,7 +17,7 @@ bool interpret_string(const std::string& str)
     return !error;
 }
 
-int main()  
+int main()
 {
     auto result = interpret_string("print(\"Hello world!\")");
     return 0;
