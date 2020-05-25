f20635bcf0286f2d4d38a234aef80c54c0883890
Mateusz Borycki <mateuszborycki@gmail.com>
2020-05-25 20:17:57 +0200

    Added lua hello world.

--- src/interpreter.cpp
+++ src/interpreter.cpp
@@ -1,6 +1,23 @@
 #include <iostream>
+#include <string>
+#include <lua.hpp>
 
-int main() {
-    std::cout << "Hello World!";
+bool interpret_string(const std::string& str)
+{
+    auto L = luaL_newstate();
+    luaL_openlibs(L);
+    auto error = luaL_dostring(L, str.c_str());
+    if (error) {
+        auto msg = lua_tostring(L, -1);
+        std::cout << "Lua error: " << msg;
+    }
+
+    lua_close(L);
+    return !error;
+}
+
+int main()  
+{
+    auto result = interpret_string("print(1)");
     return 0;
 }
